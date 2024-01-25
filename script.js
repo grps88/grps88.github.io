a0820ac708456c73c75f480624ee7b98ffdafe0f
Philip <filip.m4214@gmail.com>
2024-01-25 12:56:09 +0100

    new page structure (WIP)

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 window.onload = function() {
   document.getElementById('menu-button').addEventListener('click', function() {
     var menu = document.getElementById('menu');
-    if (menu.style.display === 'none') {
-      menu.style.display = 'block';
-    } else {
+    if (menu.style.display === 'block') {
       menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
     }
   });
 };
+
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  var timeOfDay = (hours < 12) ? "AM" : "PM";
+
+  hours = (hours > 12) ? hours - 12 : hours;
+
+  hours = (hours == 0) ? 12 : hours;
+
+  var currentTimeString = 'Jest godzina ' + hours +  ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").innerText = currentTimeString;
+}
+
+setInterval(updateClock, 1000);
